a44b7471f25a5dea72ce5a7dd5a2e75abb913c28
sr7375493@gmail.com <sr7375493@gamil.com>
2025-12-19 16:56:31 +0530

    Setup.py created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sandip Rathod",
+    author_email="rsandip3046@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
